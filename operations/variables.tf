88fb7e6a1ade95279ba42963a3dc35ba7483008f
Vigar Block <vigardev@outlook.com>
2020-12-13 11:17:22 +0800

    Add GKE infra provisioning using terraform

---
+++ operations/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  default = "setel-cluster"
+}
+
+variable "project" {
+  default = "setel-298305"
+}
+
+variable "location" {
+  default = "us-central1-c"
+}
+
+variable "initial_node_count" {
+  default = 2
+}
+
+variable "machine_type" {
+  default = "n1-highcpu-2"
+}
